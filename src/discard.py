b25505e5de58a82ac97f6417db0349063a5d5fce
e-baloo <marc.donval@gmail.com>
2024-02-10 17:02:23 +0100

    wip

--- src/discard.py
+++ src/discard.py
@@ -15,16 +15,20 @@ class Discard:
         self._discard = []
 
     def append(self, card: Card):
+        """Append a card to the discard pile."""
         card.status = CardStatusEnum.VISIBLE
         self._discard.append(card)
 
     def draw(self) -> Card:
+        """Draw a card from the discard pile."""
         return self._discard.pop()
 
     def clear(self):
+        """Clear the discard pile."""
         self._discard = []
 
     def last(self) -> Card:
+        """Return the last card in the discard pile."""
         return self._discard[-1]
 
     def __str__(self):
